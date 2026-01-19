73856ccee7d515bad03bfc34f16ac01ff1b7f5ae
Volodymyr Turanskyy <volodymyr.turanskyy@arm.com>
2026-01-19 16:00:00 +0000

    Simplified implementation of exit to avoid issues with privileged assembly instructions

--- arm-software/embedded/llvmlibc-support/crt0/crt0-nohost.cpp
+++ arm-software/embedded/llvmlibc-support/crt0/crt0-nohost.cpp
@@ -11,31 +11,11 @@ extern "C" {
 // Do nothing for platform initialization
 [[gnu::weak]] void _platform_init() {}
 
-// Define architecture specific idle mode
-#if defined(__aarch64__)
-static inline void mask_irqs(void) {
-  __asm__ volatile("msr daifset, #0xf \n isb \n" ::: "memory");
-}
-#elif defined(__arm__) && (__ARM_ARCH_PROFILE == 'M')
-static inline void mask_irqs(void) {
-  __asm__ volatile("cpsid i \n isb \n" ::: "memory");
-}
-#elif defined(__arm__)
-static inline void mask_irqs(void) {
-  __asm__ volatile("cpsid if \n isb \n" ::: "memory");
-}
-#else
-#error "Unsupported architecture"
-#endif
-
-static inline void idle_once(void) { __asm__ volatile("wfi" ::: "memory"); }
-
-// Go into low power infinite loop on exit
+// Go into busy infinite loop on exit
 [[gnu::weak, noreturn]] void __llvm_libc_exit(int status) {
   (void)status;
-  mask_irqs();
   for (;;)
-    idle_once();
+    __asm__ volatile("" ::: "memory");
 }
 
 } // extern "C"
